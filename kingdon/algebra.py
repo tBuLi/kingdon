0cb51185bbd6b88d6320e625ec18623f7ee741ba
Martin Roelfs <martin.roelfs@kuleuven.be>
2022-10-18 13:21:27 +0200

    Don't support python 3.6, do support 3.10

--- kingdon/algebra.py
+++ kingdon/algebra.py
@@ -8,7 +8,7 @@ try:
     from functools import cached_property
 except ImportError:
     from functools import lru_cache
-    
+
     def cached_property(func):
         return property(lru_cache()(func))
 
