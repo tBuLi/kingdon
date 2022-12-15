56531d049ba9401dd1f5e36abb0ee3e9fee1598d
Martin Roelfs <martin.roelfs@kuleuven.be>
2022-12-15 12:32:10 +0100

    Improved performance of operatorDict, in particulat for binary operations.

--- kingdon/operator_dict.py
+++ kingdon/operator_dict.py
@@ -14,6 +14,17 @@ class AlgebraError(Exception):
 
 @dataclass
 class OperatorDict(Mapping):
+    """
+    A dict-like object which performs codegen of a particular operator,
+    and caches the result for future use. For example, to generate the geometric product,
+    we create an OperatorDict as follows::
+
+        alg = Algebra(3, 0, 1)
+        gp = OperatorDict('gp', codegen=codegen_gp, algebra=alg)
+
+    Here, :code:`codegen_gp` is a function that outputs the keys of the result, and a callable that
+    produces the corresponding values. See :class:`~kingdon.codegen.CodegenOutput` for more info.
+    """
     name: str
     codegen: Callable
     algebra: "Algebra"
@@ -24,10 +35,13 @@ class OperatorDict(Mapping):
 
     def __getitem__(self, keys_in: Tuple[Tuple[int]]):
         if keys_in not in self.operator_dict:
-            mvs = []
-            for name, keys in zip(string.ascii_lowercase, keys_in):
-                vals = tuple(Symbol(f'{name}{self.algebra.bin2canon[ek][1:]}') for ek in keys)
-                mvs.append(MultiVector.fromkeysvalues(self.algebra, keys=keys, values=vals))
+            # Make symbolic multivectors for each set of keys and generate the code.
+            mvs = tuple(
+                MultiVector.fromkeysvalues(
+                    self.algebra, keys=keys,
+                    values=tuple(Symbol(f'{name}{self.algebra.bin2canon[ek][1:]}') for ek in keys))
+                for name, keys in zip(string.ascii_lowercase, keys_in)
+            )
             self.operator_dict[keys_in] = self.codegen(*mvs)
         return self.operator_dict[keys_in]
 
@@ -35,6 +49,9 @@ class OperatorDict(Mapping):
         return iter(self.operator_dict)
 
     def __call__(self, *mvs):
+        if len(mvs) == 2:
+            return self._call_binary(*mvs)
+
         # Make sure all inputs are multivectors. If an input is not, assume its scalar.
         mvs = [mv if isinstance(mv, MultiVector) else MultiVector.fromkeysvalues(self.algebra, (0,), (mv,))
                for mv in mvs]
@@ -43,17 +60,40 @@ class OperatorDict(Mapping):
         keys_in = tuple(mv.keys() for mv in mvs)
         values_in = tuple(mv.values() for mv in mvs)
         keys_out, func = self[keys_in]
-        values = func(*values_in)
+        values_out = func(*values_in)
 
         if self.algebra.simplify and any(mv.issymbolic for mv in mvs):
             # Keep only symbolically non-zero elements.
             keysvalues = tuple(filter(
                 lambda kv: True if not isinstance(kv[1], Expr) else simplify(kv[1]),
-                zip(keys_out, values)
+                zip(keys_out, values_out)
             ))
             keys_out, values = zip(*keysvalues) if keysvalues else (tuple(), tuple())
 
-        return MultiVector.fromkeysvalues(self.algebra, keys=keys_out, values=values)
+        return MultiVector.fromkeysvalues(self.algebra, keys=keys_out, values=values_out)
+
+    def _call_binary(self, mv1, mv2):
+        """ Specialization for binary operators. """
+        # Make sure all inputs are multivectors. If an input is not, assume its scalar.
+        mv1 = mv1 if isinstance(mv1, MultiVector) else MultiVector.fromkeysvalues(self.algebra, (0,), (mv1,))
+        mv2 = mv2 if isinstance(mv2, MultiVector) else MultiVector.fromkeysvalues(self.algebra, (0,), (mv2,))
+        # Check is written to be fast, not readable. Typically, the first check is true.
+        if not (mv1.algebra is mv2.algebra or mv1.algebra == mv2.algebra):
+            raise AlgebraError("Cannot multiply elements of different algebra's.")
+
+        keys_out, func = self[mv1.keys(), mv2.keys()]
+        values_out = func(mv1.values(), mv2.values())
+
+        if self.algebra.simplify and (mv1.issymbolic or mv2.issymbolic):
+            # Keep only symbolically non-zero elements.
+            keysvalues = tuple(filter(
+                lambda kv: True if not isinstance(kv[1], Expr) else simplify(kv[1]),
+                zip(keys_out, values_out)
+            ))
+            keys_out, values = zip(*keysvalues) if keysvalues else (tuple(), tuple())
+
+        return MultiVector.fromkeysvalues(self.algebra, keys=keys_out, values=values_out)
+
 
 class UnaryOperatorDict(OperatorDict):
     """
@@ -69,17 +109,15 @@ class UnaryOperatorDict(OperatorDict):
         return self.operator_dict[keys_in]
 
     def __call__(self, mv):
-        keys_in = mv.keys()
-        values_in = mv.values()
-        keys_out, func = self[keys_in]
-        values = func(values_in)
+        keys_out, func = self[mv.keys()]
+        values_out = func(mv.values())
 
         if self.algebra.simplify and mv.issymbolic:
             # Keep only symbolically non-zero elements.
             keysvalues = tuple(filter(
                 lambda kv: True if not isinstance(kv[1], Expr) else simplify(kv[1]),
-                zip(keys_out, values)
+                zip(keys_out, values_out)
             ))
             keys_out, values = zip(*keysvalues) if keysvalues else (tuple(), tuple())
 
-        return MultiVector.fromkeysvalues(self.algebra, keys=keys_out, values=values)
+        return MultiVector.fromkeysvalues(self.algebra, keys=keys_out, values=values_out)
