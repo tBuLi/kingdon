4fcd7faa625fdf2014f8413afdee2086b510e92f
Martin Roelfs <martin.roelfs@kuleuven.be>
2023-10-31 14:26:47 +0100

    Fixed matrix generation code and added extra test.

--- kingdon/matrixreps.py
+++ kingdon/matrixreps.py
@@ -30,7 +30,8 @@ def ordering_matrix(Rs):
     :param Rs: sequence of matrix reps for all the basis blades of an algebra.
     :return: The similarity transform to beat all similarity transforms.
     """
-    return np.array([[Ri[j, 0] for Ri in Rs] for j in range(len(Rs[0]))])
+    columns = [Ri[:, 0] for Ri in Rs]
+    return np.vstack(columns)
 
 
 def matrix_rep(p=0, q=0, r=0):
@@ -45,7 +46,7 @@ def matrix_rep(p=0, q=0, r=0):
     :param r: number of null dimensions.
     :return: sequence of matrix reps for the basis-blades.
     """
-    d = p+q+r
+    d = p + q + r
     I = I2
     P = P2
     Z = Z2
@@ -76,4 +77,4 @@ def matrix_rep(p=0, q=0, r=0):
         Rs.extend(Rs_grade_i)
 
     O = ordering_matrix(Rs)
-    return [O@Ri@O.T for Ri in Rs]
+    return [O @ Ri @ O.T for Ri in Rs]
