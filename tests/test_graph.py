cfada70f9684cc5224757d4ae006001488765a68
Martin Roelfs <martin.roelfs@kuleuven.be>
2024-04-30 17:40:15 +0200

    Added test to ensure the type of signature is always int.

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -9,4 +9,5 @@ def test_widget():
     g = alg.graph(*args)
     assert g.draggable_points_idxs == [1]
     assert g.draggable_points == [[{'keys': x.keys(), 'mv': x.values()}]]
+    assert all(type(s) == int for s in g.signature)
 
