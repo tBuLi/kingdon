5da4544f9f78f1b3860db38d488254b73e20f1c1
Martin Roelfs <martin.roelfs@kuleuven.be>
2024-10-23 15:39:07 +0200

    First try at including custom basis order in kingdon.

--- kingdon/algebra.py
+++ kingdon/algebra.py
@@ -111,6 +111,7 @@ class Algebra:
     # Mappings from binary to canonical reps. e.g. 0b01 = 1 <-> 'e1', 0b11 = 3 <-> 'e12'.
     canon2bin: dict = field(init=False, repr=False, compare=False)
     bin2canon: dict = field(init=False, repr=False, compare=False)
+    # basis2canon: dict = field(init=False, repr=False, compare=False)
     _bin2canon_prettystr: dict = field(init=False, repr=False, compare=False)
 
     # Options for the algebra
@@ -241,22 +242,6 @@ class Algebra:
     @cached_property
     def matrix_basis(self):
         return matrix_rep(self.p, self.q, self.r)
-
-    @cached_property
-    def frame(self) -> list:
-        r"""
-        The set of orthogonal basis vectors, :math:`\{ e_i \}`. Note that for a frame linear independence suffices,
-        but we already have orthogonal basis vectors so why not use those?
-        """
-        return [self.blades[self.bin2canon[2**j]] for j in range(0, self.d)]
-
-    @cached_property
-    def reciprocal_frame(self) -> list:
-        r"""
-        The reciprocal frame is a set of vectors :math:`\{ e^i \}` that satisfies
-        :math:`e^i \cdot e_j = \delta^i_j` with the frame vectors :math:`\{ e_i \}`.
-        """
-        return [v.inv() for v in self.frame]
 
     def _prepare_signs_and_cayley(self):
         r"""
