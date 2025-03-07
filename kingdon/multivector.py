588b3388279a6cfa7d6e9a0452201f1101ffb762
Martin Roelfs <martinroelfs@yahoo.com>
2025-03-07 17:25:01 +0100

    Don't break sympify for Symbol subclasses.

--- kingdon/multivector.py
+++ kingdon/multivector.py
@@ -84,7 +84,7 @@ class MultiVector:
             keys = tuple(key if key in algebra.bin2canon else algebra.canon2bin[key]
                          for key in keys)
 
-        if isinstance(symbolcls, Symbol) and any(isinstance(v, str) for v in values):
+        if issubclass(symbolcls, Symbol) and any(isinstance(v, str) for v in values):
             values = list(val if not isinstance(val, str) else sympify(val)
                           for val in values)
 
